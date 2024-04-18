5a2ef13355d95afc876ff78f247ee53b087a4c1d
Bruno Cruz <112664341+BrunoJss@users.noreply.github.com>
2024-04-18 08:17:53 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-async def root():
+def root():
     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
 
 @app.get("/pira")
-async def test():
+def test():
     return [{"empresa":"PIRACICABANA","cartao":"3587921","tipo":"FUNCIONARIO"},{"empresa":"BR MOBILIDADE","cartao":"290963","tipo":"PASSE LIVRE"},{"empresa":"BR MOBILIDADE","cartao":"8566978","tipo":"COMUM"}]
 
 @app.get("/br")
-async def test2():
+def test2():
     return {{"empresa":"PIRACICABANA","cartao":"3587921","tipo":"FUNCIONARIO"},{"empresa":"BR MOBILIDADE","cartao":"290963","tipo":"PASSE LIVRE"},{"empresa":"BR MOBILIDADE","cartao":"8566978","tipo":"COMUM"}}
